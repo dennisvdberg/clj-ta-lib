336127c9a370b14bd8a513725a18510b97907c54
Dennis van den Berg <dennisvdberg@gmail.com>
2017-01-09 08:14:37 +0100

    added clojure memoize

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [junit/junit "4.11"]]
+                 [junit/junit "4.11"]
+                 [org.clojure/core.memoize "0.5.8"]]
   :source-paths      ["src/main/clojure"]
   :java-source-paths ["src/main/java"])
